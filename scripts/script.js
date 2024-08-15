d687968935abd57df63bf985c886a3d506874345
Felix Mangold <felix.mangold@student.uni-luebeck.de>
2024-08-15 16:22:02 +0200

    added function setGalleryImages

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,33 @@ const fetchImages = async (page = 1) => {
     // parse json from fetched data
     const json = await response.json();
     
+    console.log(json);
     return json;
   }
   // catch & log error
   catch (error) {
     console.error(error.message);
   }
+}
+
+const setGalleryImages = async (page = 1) => {
+  const images = await fetchImages(page);
+  const gallery = document.getElementById('gallery');
+
+  for(const image of images) {
+  
+    console.log(image);
+
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+
+    div.className = 'item-gallery';
+
+    img.src = image.download_url;
+    img.alt = 'Photo by ' + image.author;
+
+    div.appendChild(img);
+    gallery.appendChild(div);
+
+  };
 }
